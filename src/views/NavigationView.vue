2d8aa0d8290951898eb06f8db6d5bb2f75b2d0b5
Lennart Becker <lennart.becker@outlook.com>
2023-07-28 15:51:18 +0200

    ADD: NavigtaionView

---
+++ src/views/NavigationView.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/galery">Galery</router-link> |
+        <router-link to="/about">About</router-link> |
+        <router-link to="/impressum">impressum</router-link> |
+    </nav>
+  </template>
+  
+  <script>
+  // @ is an alias to /src
+  
+  export default {
+    name: 'navigation-view',
+    components: {
+    }
+  }
+  </script>
+  
+  <style scoped>
+  
+  </style>
